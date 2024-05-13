0636e412e00ec75e1b90c0b23988b128f713a65a
jiin <jyl08840@naver.com>
2024-05-13 22:05:20 +0900

    5/13 설정 및 테스트코드 완료

---
+++ src/test/java/org/zerock/ex00/sample/DITests.java
@@ -0,0 +1,43 @@
+package org.zerock.ex00.sample;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class DITests {
+
+    @Autowired
+    DataSource dataSource;
+
+
+    @Autowired
+    Restaurant restaurant;
+
+
+    @Test
+    public  void testCon() throws Exception{
+
+        Connection con = dataSource.getConnection();
+
+        System.out.println(con);
+
+        con.close();
+
+    }
+
+
+    @Test
+    public void testExist(){
+
+        System.out.println(restaurant);
+    }
+
+}
